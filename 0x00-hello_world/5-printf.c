206623274325defabd64d7a7b919b8ae1105cb45
salmahosny22 <salmahosny212@gmail.com>
2023-05-21 11:28:59 -0700

    5 - print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *
  * main - Entry
  * Return: Always 0
  */
@@ -9,3 +8,4 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
