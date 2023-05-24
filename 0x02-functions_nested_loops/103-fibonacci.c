7c47497d653719fe245964a329fd0ca3c3862ab4
salmahosny22 <salmahosny212@gmail.com>
2023-05-24 12:34:16 -0700

    103 fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Prints the sum of even-valued Fibonacci sequence
+ * maini - Prints the sum of even-valued Fibonacci sequence
  *        terms not exceeding 4000000.
  *
  * Return: Always 0.
